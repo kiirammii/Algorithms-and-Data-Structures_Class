8349da83e7f1560ed513ce9ad90ee7dfa60fdf8e
kiirammii <catarinatsousa04@gmail.com>
2023-10-16 16:26:48 +0100

    exercicio 7 !!!

---
+++ FICHA 3/ex_7.py
@@ -0,0 +1,13 @@
+number = int(input("numero: "))
+
+if number <= 1:
+    print('inválido')
+else:
+    for i in range (2, number):
+        if number % i == 0:
+            print ("o numero", number, "não é primo")
+            break
+        else:
+            print("o numero", number, "é primo")
+            break
+    
