375ca95e9300f2f2eddd5a1903ff90f7256257a8
kiirammii <catarinatsousa04@gmail.com>
2023-10-18 15:50:55 +0100

    -

--- FICHA 3/ex_11.py
+++ FICHA 3/ex_11.py
@@ -1,12 +1,8 @@
-import math
-import sys
-
 quantidade = int(input("quantos números pretende inserir?: "))
-maior = sys.float_info.max
 segundo_maior = 0
 
 if quantidade <= 0:
-    print("quantidade inválida")
+    print("inválida")
 else:
     for i in range (1, quantidade+1):
         number = int(input("número: "))
