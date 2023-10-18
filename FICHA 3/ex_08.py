f5a925356f925e8348da4ef93947647000654c0b
kiirammii <catarinatsousa04@gmail.com>
2023-10-18 16:23:02 +0100

    -

--- FICHA 3/ex_08.py
+++ FICHA 3/ex_08.py
@@ -1,6 +1,8 @@
 quantidade = int(input("quantos números na sequência?: "))
 antepenultimo = 0
 penultimo = 1
+ultimoTermo = 0
+sequencia = ''
 
 if quantidade == 0:
     print("inválido")
@@ -9,3 +11,10 @@ elif quantidade == 1:
 elif quantidade == 2:
     print(penultimo)
 else:
+    sequencia += str(antepenultimo) + '  ' + str(penultimo)
+    for i in range (3, quantidade+1):
+        ultimoTermo = int(antepenultimo) + int(penultimo)
+        sequencia += '  ' + str(ultimoTermo)
+        antepenultimo = penultimo
+        penultimo = ultimoTermo
+    print (sequencia)
