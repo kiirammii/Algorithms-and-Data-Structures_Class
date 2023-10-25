f6bf4a8b78148d19e1aa6387b69e129bad8d8f78
kiirammii <catarinatsousa04@gmail.com>
2023-10-25 10:47:51 +0100

    -

--- FICHA 04/ex_04.py
+++ FICHA 04/ex_04.py
@@ -1,4 +1,16 @@
-def valorMedio(*numeros):
-    total = 0
-    #soma
-    media = total / len(numeros)
+def calculteAverage(*numeros):
+    if len(numeros) == 0:
+        return 0
+    
+    else:
+        total = 0
+        for num in numeros:
+            total += num
+        
+        average = total / len(numeros)
+        return average
+
+average1 = calculteAverage(2,3,4)
+average2 = calculteAverage(2,8,10,6,14)
+print("Average 1:", average1)
+print("Average 2:", average2)
