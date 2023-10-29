13a3933e683321e4ece661e059f414f43ca1d53f
kiirammii <catarinatsousa04@gmail.com>
2023-10-29 17:02:54 +0000

    -

--- FICHA 05/ex_09.py
+++ FICHA 05/ex_09.py
@@ -1,15 +1,23 @@
 def countWord(text, word):
-    quantity = 0
-    counter = text.count('vehicle')
-    posX = 0
+    occurrences = 'Posições: '
+    count = 0
+    start = 0
 
-    for i in range (1, counter):
-        pos = text.find('vehicle', posX)
-        quantity += 1
-        posX = pos+1
+    while True:
+        index = text.find(word, start)
 
-    print(counter)
-    return quantity
+        if index == -1:
+            break
 
-text = 'Using a GPS/GPRS unit installed in each vehicle, a monitoring device  records real-time  information about the location of  each vehicle'
-print(countWord(text, 'vehicle'))
+        if count > 0:
+            occurrences += ", "
+
+        occurrences += str(index)
+        count += 1
+        start = index + 1
+
+    return count , occurrences
+
+text = "Using a GPS/GPRS unit installed in each vehicle, a monitoring device records real-time information about the location of each vehicle."
+word = 'vehicle'
+print(countWord(text, word))
