142e0cfa7404c2018acfeea3b1eace8671cb3886
kiirammii <catarinatsousa04@gmail.com>
2023-11-06 15:14:34 +0000

    -

--- FICHA 05/ex_10.py
+++ FICHA 05/ex_10.py
@@ -4,7 +4,7 @@ def printCharLine (text, num):
     """esta função imprime n numero de caracteres de um texto por linha"""
 
     # determina quantos ciclos terá de fazer (linhas)
-    number = len(text)/num # divide o numero de caracteres, pelo numero de caracteres por linha pedidos
+    number = len(text)/num # divide o numero de caracteres do texto, pelo numero de caracteres por linha pedidos
     ciclos = math.ceil(number) # arredonda o valor para cima
 
     min = 0 # começa no 0
@@ -12,10 +12,10 @@ def printCharLine (text, num):
 
     for i in range (ciclos): # repete o ciclo para todas as linhas do texto
         line = text[min:max] # primeira linha vai desde o valor minimo até ao valor n pedido
+        print(line) # imprime a linha
+
         min += num # adiciona num ao valor minimo
         max += num # adiciona num ao valor maximo
-        
-        print(line) # imprime a linha
 
 text = 'este e um texto para verificar o exercicio 10 da ficha 5 de AED'
 num = 10 # numero de caracteres por linha
