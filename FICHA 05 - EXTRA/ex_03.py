605e5f6e4070704b4d1f88f2cbcf2208cb3e62b6
kiirammii <catarinatsousa04@gmail.com>
2023-11-08 17:40:22 +0000

    -

--- FICHA 05 - EXTRA/ex_03.py
+++ FICHA 05 - EXTRA/ex_03.py
@@ -1,4 +1,18 @@
 def countRepeat(text):
     text = text.lower()
 
-    
+    words = text.split(' ')
+
+    uniqueWords = []
+    repeatedWords = []
+
+    for word in words:
+        if word in uniqueWords and word not in repeatedWords:
+            repeatedWords.append(word)
+        else:
+            uniqueWords.append(word)
+
+    return repeatedWords
+
+text = 'Exercicio de AED parecido com o exercicio do teste de AED'
+print(countRepeat(text))
